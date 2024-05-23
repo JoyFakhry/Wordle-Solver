501040a6c61d0d72147e2ede7785dabce9c0843f
JoyFakhry <joyfakhry1999@gmail.com>
2024-05-22 20:28:36 -0400

    update

--- Wordle_Solver.py
+++ Wordle_Solver.py
@@ -1,7 +1,14 @@
 import random
 import string
-from big_list import valid_WORDS, frequency_WORDS    # cached list of all valid words and dict of frequency of words
+# cached list of all valid words and dict of frequency of words
+from big_list import valid_WORDS, frequency_WORDS
 from termcolor import colored   # pip install termcolor to work
+
+
+# import sys
+# import subprocess
+# subprocess.check_call([sys.executable, '-m', 'pip', 'install',
+#    'termcolor'])
 
 
 # function to check if a word in word list matches user guess
@@ -16,12 +23,13 @@ def check_guess(guess):
             u_letters.add(letter)
             return False
     for index, letter in enumerate(g_tiles):    # check green tiles next
-        if (letter != '') and (guess[index] != letter): # if green tile not in same position as word
+        # if green tile not in same position as word
+        if (letter != '') and (guess[index] != letter):
             return False
     keep = [True]   # initialize list for each letter in yellow tiles
     for index, letter in enumerate(y_tiles):
         if (letter in u_tiles) or (letter in g_tiles):  # if letter occurs twice in guess
-            if guess.count(letter) < 2: # if letter occurs once in guess
+            if guess.count(letter) < 2:  # if letter occurs once in guess
                 return False
         if (letter != '') and (letter not in guess):    # if letter not in word
             return False
@@ -34,7 +42,8 @@ def check_guess(guess):
 def random_guess(word_list):
     a_word = random.choice(word_list)
     if len(word_list) > 20:     # if there are a lot of possible words
-        while len(a_word) != len(set(a_word)):  # loop through word until you don't get repeating letters
+        # loop through word until you don't get repeating letters
+        while len(a_word) != len(set(a_word)):
             a_word = random.choice(word_list)   # get random choice from list
     return a_word
 
@@ -54,18 +63,23 @@ def possible_guesses(list_of_lengths):
     print("\nPossibilities History:")
     for index, length in enumerate(list_of_lengths):
         print(f"Guess {index}: {length}")
-        
+
+
 def letters_used():
     sorted_set = sorted(u_letters)
-    print(f"Grayed out letters (don't guess words with these {len(sorted_set)} letters): {''.join(sorted_set)}")
+    print(
+        f"Grayed out letters (don't guess words with these {len(sorted_set)} letters): {''.join(sorted_set)}")
+
 
 if __name__ == "__main__":
- 
+
     print("Wordle Solver\n")
 
-    print('Random first guess:', colored(f'{random_guess(valid_WORDS).upper()}', 'red'))
+    print('Random first guess:', colored(
+        f'{random_guess(valid_WORDS).upper()}', 'red'))
     previous_possibilities = valid_WORDS    # initialize to whole valid word list
-    len_of_possibilities = [len(valid_WORDS)]   # initialize to length of whole valid word list
+    # initialize to length of whole valid word list
+    len_of_possibilities = [len(valid_WORDS)]
 
     u_letters = set()  # initialize to empty set of grey letters
     guess_number = 1    # guess number counter initialization
@@ -80,21 +94,24 @@ if __name__ == "__main__":
         y_tiles = []
 
         # get user input
-        print("Enter " + colored("green ", "green") + "letters (return/enter for empty)")
+        print("Enter " + colored("green ", "green") +
+              "letters (return/enter for empty)")
         g_tiles.append(input(colored('Green 1: ', 'green')))
         g_tiles.append(input(colored('Green 2: ', 'green')))
         g_tiles.append(input(colored('Green 3: ', 'green')))
         g_tiles.append(input(colored('Green 4: ', 'green')))
         g_tiles.append(input(colored('Green 5: ', 'green')))
 
-        print("Enter " + colored("yellow ", "yellow") + "letters (return/enter for empty)")
+        print("Enter " + colored("yellow ", "yellow") +
+              "letters (return/enter for empty)")
         y_tiles.append(input(colored('Yellow 1: ', 'yellow')))
         y_tiles.append(input(colored('Yellow 2: ', 'yellow')))
         y_tiles.append(input(colored('Yellow 3: ', 'yellow')))
         y_tiles.append(input(colored('Yellow 4: ', 'yellow')))
         y_tiles.append(input(colored('Yellow 5: ', 'yellow')))
 
-        print("Enter " + colored("gray ", "grey") + "letters (return/enter for empty)")
+        print("Enter " + colored("gray ", "grey") +
+              "letters (return/enter for empty)")
         u_tiles.append(input(colored('Gray 1: ', 'grey')))
         u_tiles.append(input(colored('Gray 2: ', 'grey')))
         u_tiles.append(input(colored('Gray 3: ', 'grey')))
@@ -103,7 +120,7 @@ if __name__ == "__main__":
 
         # get list of possible words that match current guess and possible words
         possibilities = list(filter(check_guess, previous_possibilities))
-        len_of_possibilities.append(len(possibilities))    
+        len_of_possibilities.append(len(possibilities))
         possible_guesses(len_of_possibilities)  # print possibilities history
         letters_used()  # print grey letters found so far
         next_guess = frequency_guess(possibilities)
@@ -117,7 +134,8 @@ if __name__ == "__main__":
                     continue
                 else:
                     print(word.upper())
-        previous_possibilities = possibilities  # set next rounds word list to this rounds solutions
+        # set next rounds word list to this rounds solutions
+        previous_possibilities = possibilities
         if len_of_possibilities[-1] != 1:
             guess_number += 1   # increment guess counter when there needs to be another guess
 
